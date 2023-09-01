37aaace1b0dab64babbee38b242f91654b6223b0
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2023-09-01 20:51:17 +0500

    Добавлены материалы занятия №7 (лекция №3)

---
+++ lesson7_lection/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="NEW Item"/>
+    <CalcComponent/>
+    <h2>NEW CALCULATOR</h2>
+    <CalcComponent/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import CalcComponent from './components/CalcComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    CalcComponent
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
