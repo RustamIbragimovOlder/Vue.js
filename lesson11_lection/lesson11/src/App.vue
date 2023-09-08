c595cd55debfe52ffd011c26bb35089ad16837a9
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2023-09-08 12:26:20 +0500

    Добавлены материалы занятия №11 (лекция №6)

---
+++ lesson11_lection/lesson11/src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+    <div>
+      <header>
+        <div>My personal costs</div>
+      </header>
+      <main>
+        <PaymentsDisplayComp :items="paymentList" :info="infoText" />
+        <AddListComp @addNewPay="addNewPay" />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import PaymentsDisplayComp from './components/PaymentsDisplayComp.vue';
+import AddListComp from './components/AddListComp.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    PaymentsDisplayComp,
+    AddListComp
+  },
+
+  data() {
+    return {
+      paymentList: [],
+    };
+  },
+
+  methods: {
+    fetchData() {
+      return [
+        {
+          date: '05.09.2023',
+          category: 'Food',
+          value: 1217,
+        },
+        {
+          date: '05.09.2023',
+          category: 'Gift',
+          value: 5075,
+        },
+        {
+          date: '06.09.2023',
+          category: 'Building materials',
+          value: 1999,
+        },
+        {
+          date: '06.09.2023',
+          category: 'Transport',
+          value: 998.50,
+        },
+        {
+          date: '07.09.2023',
+          category: 'Building materials',
+          value: 582.75,
+        },
+        {
+          date: '07.09.2023',
+          category: 'Building materials',
+          value: 13750,
+        },
+      ]
+    },
+    addNewPay(data) {
+      this.paymentList.push(data)
+
+    }
+  },
+
+  created() {
+    this.paymentList = this.fetchData();
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lesson11_lection/lesson11/src/components/PaymentsDisplayComp.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div v-for="item in items" :key="item.id">
+            {{ item }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PaymentsDisplayComp',
+
+    props: {
+        items: {
+            type: Array,
+            default() {
+                return [];
+            },
+        },
+    },
+
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+
+};
+</script>
+
+<style lang="scss" scoped></style>
